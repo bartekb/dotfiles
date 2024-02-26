398874cf93106b318ff25f2d9c763e491d273f98
Bartosz Bubacz <bartosz.bubacz@gmail.com>
2024-02-26 23:00:37 +0100

    Starship added

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,6 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.font_size = 18
 
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
 -- For example, changing the color scheme:
 config.color_scheme = "Ayu Mirage (Gogh)"
 
